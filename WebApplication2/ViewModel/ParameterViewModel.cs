12316a860248567391e6215cc26d844c2fb62d64
oscar martinez <oscarmartinez2010@gmail.com>
2017-05-02 22:25:14 -0500

    Initial layout of project, still need to set up footer and example page

---
+++ WebApplication2/ViewModel/ParameterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.ViewModel
+{
+    public class ParameterViewModel
+    {
+        public string ParmId { get; set; }
+        public string ParmName { get; set; }
+        public string Program { get; set; }
+        public string Technology { get; set; }
+    }
+}
