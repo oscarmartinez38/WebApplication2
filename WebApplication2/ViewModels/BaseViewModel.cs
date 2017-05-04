6e4a7199b6b01268a41d085566017bb317681294
oscar martinez <oscarmartinez2010@gmail.com>
2017-05-03 20:40:04 -0500

    Added home page, some of the header and footer

---
+++ WebApplication2/ViewModels/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.ViewModels
+{
+    public class BaseViewModel
+    {
+        public string UserName { get; set; }
+    }
+}
